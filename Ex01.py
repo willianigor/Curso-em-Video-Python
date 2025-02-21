d266db0f2f5855034d5051d7868f66f4ea55217a
Igor Willian <igorwillian2023@gmail.com>
2025-02-21 08:59:25 -0300

    First Commit

---
+++ Ex01.py
@@ -0,0 +1,7 @@
+num1 = float(input('Digite o primeiro numero\n'))
+num2 = float(input('Digite o segundo numero\n'))
+soma = num1 + num2
+
+print('A soma equivale a: ',soma)
+
+input('Pressione enter para sair')
---
+++ Ex02.py
@@ -0,0 +1,8 @@
+diaNascimento = input('Que dia voce nasceu?\n')
+mesNascimento = input('De qual mes?(Digite apenas o numero do mes)\n')
+anoNascimento = input('E foi em que ano?\n')
+nome = input('Qual seu nome?\n')
+
+print('Muito bem,',nome,',voce nasceu em',diaNascimento,'/',mesNascimento,'/',anoNascimento)
+
+input('Pressione enter para sair')
