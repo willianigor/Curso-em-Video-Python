0e717a5d427a48eabe4f1ae876fa34e7ba22e69c
Igor Willian <igorwillian2023@gmail.com>
2025-02-23 11:48:59 -0300

    Aula 11(Uso de cores no terminal)

---
+++ Aula11.py
@@ -0,0 +1,4 @@
+#Nessa aula, vamos aprender como utilizar os códigos de escape sequence ANSI para configurar cores para os seus programas em Python.
+#Veja como utilizar o código \033[m com todas as suas principais possibilidades.
+print('\033[1;32;42mOla mundo\033[m\n')
+print('\033[1;31;41mOla mundo\033[m\n')
